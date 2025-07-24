1c43cd9e20f0c0295b1afbef4e320386db15f842
manoj <manojgoud335@gmail.com>
2025-07-25 03:02:58 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,53 @@
+import { useState } from 'react'
+import './App.css'
+import data from './data'
+
+const App = () => {
+  const [people, setPeople] = useState(data)
+  console.log(data)
+
+  return (
+    <div className='container'>
+      <h1  className='head'>Birthday Party App</h1>
+      <p className='para'>
+        <span
+          className='length'
+          style={people.length === 0 ? { color: 'red' } : { color: 'green' }}
+        >
+          {people.length}
+        </span>
+         Birthday Party</p>
+      <div className='people'>
+        {people.map((person) => {
+          // console.log(person)
+          const { age, id, image, name } = person
+          // console.log(age,id,image,name)
+          return (
+            
+            <div className='person' key={id}>
+              <img src={image} alt={name} className='person-image' />
+              <div className='person-info'>
+                <h4 className='person-name'>{name}</h4>
+                <p className='person-age'>Age: {age}</p>
+                </div>
+                </div>
+          );
+        })}
+    </div>
+    <div className='buttons'>
+
+      {people.length > 1 && <button className='btn' onClick={() => setPeople([])}>
+        Clear All
+      </button>}
+      {people.length == 0 &&<button className='btn' onClick={() => setPeople(data)}>
+        Refresh
+      </button>}
+    </div>
+      <p className='footer'>Made with ❤️ by Manoj kumar</p>
+      <p className='footer'>© 2023 Birthday Party App</p>
+
+    </div>
+  );
+}
+
+export default App
